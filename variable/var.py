c031b9c764b662818ae2cb5500fa3caec53d01da
Maria-Liaqat-cmd <marialiaqat68@gmail.com>
2024-12-19 01:36:18 -0800

    Add files via upload

---
+++ variable/var.py
@@ -0,0 +1,21 @@
+print("hello world")
+
+num1 = 10
+num2 = 20
+result= num1+num2
+print(result)
+
+a= 20
+b= 30
+c= a-b
+print(c)
+
+c=20
+d=40
+result= c*d
+print(result)
+
+e=30
+s=15
+result=e/s
+print(result)
